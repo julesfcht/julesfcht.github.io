62cb9bb5c859f63386d47b86ac11386e14dba1f3
Jules <jules.fichot@hotmail.fr>
2021-03-17 23:19:29 +0100

    Updates

---
+++ precache-manifest.844ca953372cc4d44bf4abcf10fd7388.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1a97d133c3885943b048e6948be338",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db92e78fdd54de42e3e",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "b534756fdf8438e51b81",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "76e03fc167f106d5067f",
+    "url": "/static/css/main.207db4cf.chunk.css"
+  },
+  {
+    "revision": "476c5ea995c092c042b3",
+    "url": "/static/js/2.4786cc38.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.4786cc38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db92e78fdd54de42e3e",
+    "url": "/static/js/3.4a0ebc8c.chunk.js"
+  },
+  {
+    "revision": "b534756fdf8438e51b81",
+    "url": "/static/js/4.53a77286.chunk.js"
+  },
+  {
+    "revision": "76e03fc167f106d5067f",
+    "url": "/static/js/main.bb3cc5b4.chunk.js"
+  },
+  {
+    "revision": "517180b60c759a5a0e7f",
+    "url": "/static/js/runtime-main.1900a8b7.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "7ab4846aec47ed5ea7675688625aaf4a",
+    "url": "/static/media/ministere-economie.7ab4846a.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "a363b0b1f14c8469b986f221d2889eab",
+    "url": "/static/media/universite-nantes.a363b0b1.png"
+  }
+]);
