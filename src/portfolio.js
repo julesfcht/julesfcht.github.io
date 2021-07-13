25742eade40f2a6b5e1fdda7cdac11d6ae2cd691
Jules Fichot <jules.fichot@hotmail.fr>
2021-07-13 15:01:37 +0200

    first changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -17,7 +17,7 @@ const greeting = {
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing",
+    "https://drive.google.com/file/d/1acxNifHuMcK3heEvk5hbvENtc21-UhmC/view?usp=sharing",
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
